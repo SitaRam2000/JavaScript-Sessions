6aaf2510bdbc937428f5f3e268533fd3104b365c
SitaRam2000 <75799590+SitaRam2000@users.noreply.github.com>
2020-12-10 21:27:09 +0530

    Add files via upload

---
+++ session1.js
@@ -0,0 +1,54 @@
+//Variables
+//Printing output to console
+//operators and datatypes
+//few examples
+
+// var a = 10;
+// var b = 20;
+// let c = 30;
+
+// console.log(a);
+// console.log("Hello");
+// console.log("Good Morning");
+// The value of a is 10
+// console.log("The value of a is " + a);
+// console.log(`The value of a is ${a}`);
+// console.log(`the value of  c is ${c}`);
+
+// numbers
+// strings
+// boolean true/false
+// float 2.45
+// characters
+// var s = "hello";
+// console.log(typeof s);
+
+//Operators +,-,*,/,%,=,==
+
+let a = 20;
+let b = 30;
+let c = 10;
+result = (a + b) * c; //500 // operator precedence(*,/,+,-)
+result1 = (a + b - c) * a; // -150
+console.log(`Result: ${result1}`);
+
+console.log(`Result: ${result}`);
+// sum = a + b;
+// sub = a - b;
+// multiply = a * b;
+// division = a / b;
+// reminder = b % a;
+
+// console.log(`Sum of Numbers: ${sum}`);
+// console.log(`Sub of Numbers: ${sub}`);
+// console.log(`multiply of Numbers: ${multiply}`);
+// console.log(`division of Numbers: ${division}`);
+// console.log(`reminder of Numbers: ${reminder}`);
+
+// Average of three numbers
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// sum = a + b + c;
+// Average = sum / 3;
+// console.log(`Average of three numbers is: ${Average}`);
